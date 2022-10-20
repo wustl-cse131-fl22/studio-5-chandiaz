607fdd38fddbe081a17f0dfa378cf6342c23f368
tracy <tracy@localhost>
2022-10-20 16:51:57 -0500

    done

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -1,12 +1,21 @@
 package studio5;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.awt.Color;
+import java.util.Arrays;
+
 import edu.princeton.cs.introcs.StdDraw;
 
+/**
+ * 
+ * @author tracy
+ *
+ */
 public class Methods {
 
 	/**
 	 * Compute the (Euclidean) distance between two points.
-	 *
 	 * @param x1 x-coordinate of a point
 	 * @param y1 y-coordinate of a point
 	 * @param x2 x-coordinate of another point
@@ -16,13 +25,12 @@ public class Methods {
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		distance = Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
 		return distance;
 	}
 
 	/**
 	 * Draw a bull's eye at the given location with the given radius.
-	 *
 	 * @param x      the x coordinate of the center of the bull's eye
 	 * @param y      the y coordinate of the center of the bull's eye
 	 * @param radius the radius of the bull's eye
@@ -30,20 +38,29 @@ public class Methods {
 	public static void drawBullsEye(double x, double y, double radius) {
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.filledCircle(x, y, radius);
+		
 
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		Color c1 = new Color(0, 109, 219);
+		StdDraw.setPenColor(c1);
+		StdDraw.filledCircle(x, y, 0.75*radius);
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
+		Color c2 = new Color(146, 0, 0);
+		StdDraw.setPenColor(c2);
+		StdDraw.filledCircle(x, y, 0.5*radius);
 
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
+		Color c3 = new Color(255, 255, 109);
+		StdDraw.setPenColor(c3);
+		StdDraw.filledCircle(x, y, 0.25*radius);
 
 		
 	}
@@ -51,31 +68,38 @@ public class Methods {
 	/**
 	 * Return a new String which is the original source String with all occurrences
 	 * of the target character substituted by the replacement String.
-	 * 
 	 * @param source      the source String
 	 * @param target      the target character to be replaced
 	 * @param replacement the replacement String
-	 * 
 	 * @return the String which results from substituting all of the target
 	 *         characters in the source String with the replacement String
 	 */
+
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
 		// TODO: Finish this method
-		
+		for (int i = 0; i < source.length(); i++) {
+			char c = source.charAt(i);
+			if (c == target) {
+				result = result + replacement;
+			}else {
+				result = result + c;
+			}
+		}
 		return result;
 	}
 
 	/**
 	 * Compute the sum of elements in an array
-	 * 
 	 * @param values an array of integers
 	 * @return the sum of the elements in values
 	 */
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		for (int i = 0; i < values.length; i++) {
+			sum += values[i];
+		}
 		return sum;
 	}
 
@@ -87,16 +111,27 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		 // FIXME: Create an array of the appropriate size
+		int[] array = new int[length];
 		// TODO: Finish this method
-
-		
-
-		return values;
+		Arrays.fill(array, value);
+		return array;
 	}
 
-	// TODO: Create an arrayMean method which accepts an int array of values parameter.
-	// TODO: Create a JavaDoc comment for the arrayMean method.
+	
+	/**
+	 * return the average value of the input array
+	 * @param values an array
+	 * @return a double (the average value of the input array)
+	 */
+	public static double arrayMean(int[] values) {
+//		double sum = arraySum(values);
+//		double mean = sum / values.length;
+		return ((double)arraySum(values))/values.length;
+	}
+	
+	
+	
 
 	
 }
